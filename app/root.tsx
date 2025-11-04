b32933c85d94232aa494de858ac58a1f53e496ea
Daniel Mai <danielmai1728@gmail.com>
2025-11-03 16:41:03 -0800

    initial commit

--- app/root.tsx
+++ app/root.tsx
@@ -1,18 +1,32 @@
 import {
   Form,
+  Link,
+  Outlet,
   Scripts,
   ScrollRestoration,
   isRouteErrorResponse,
 } from "react-router";
+import { getContacts } from "./data";
 import type { Route } from "./+types/root";
 
 import appStylesHref from "./app.css?url";
 
-export default function App() {
+export async function clientLoader() {
+  const contacts = await getContacts();
+  return { contacts };
+}
+
+
+export default function App({ loaderData }: Route.ComponentProps) {
+  const contacts = loaderData?.contacts || [];
+
   return (
     <>
       <div id="sidebar">
         <h1>React Router Contacts</h1>
+        <h1>
+          <Link to="about">About</Link>
+        </h1>
         <div>
           <Form id="search-form" role="search">
             <input
@@ -29,15 +43,34 @@ export default function App() {
           </Form>
         </div>
         <nav>
-          <ul>
-            <li>
-              <a href={`/contacts/1`}>Your Name</a>
-            </li>
-            <li>
-              <a href={`/contacts/2`}>Your Friend</a>
-            </li>
-          </ul>
+           {contacts.length ? (
+            <ul>
+              {contacts.map((contact) => (
+                <li key={contact.id}>
+                  <Link to={`contacts/${contact.id}`}>
+                    {contact.first || contact.last ? (
+                      <>
+                        {contact.first} {contact.last}
+                      </>
+                    ) : (
+                      <i>No Name</i>
+                    )}
+                    {contact.favorite ? (
+                      <span>★</span>
+                    ) : null}
+                  </Link>
+                </li>
+              ))}
+            </ul>
+          ) : (
+            <p>
+              <i>No contacts</i>
+            </p>
+          )}
         </nav>
+      </div>
+      <div id="detail">
+        <Outlet />
       </div>
     </>
   );
@@ -91,5 +124,14 @@ export function ErrorBoundary({ error }: Route.ErrorBoundaryProps) {
         </pre>
       )}
     </main>
+  );
+}
+
+export function HydrateFallback() {
+  return (
+    <div id="loading-splash">
+      <div id="loading-splash-spinner" />
+      <p>Loading, please wait...</p>
+    </div>
   );
 }
